5648c5d2927c09b08e64b94beb8a3f9a6d0c5241
luisiy01 <luisnunez91@gmail.com>
2025-10-07 10:50:23 -0600

    add landing page

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,18 @@ import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validation';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
       validationSchema: JoiValidationSchema,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
 
     MongooseModule.forRoot(

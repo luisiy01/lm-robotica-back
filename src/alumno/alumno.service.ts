b7d470f60a2d72d735464b3bd71ffc6813a80911
luisiy01 <luisnunez91@gmail.com>
2025-10-07 10:04:32 -0600

    create alumno endpoint

--- src/alumno/alumno.service.ts
+++ src/alumno/alumno.service.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreateAlumnoDto } from './dto/create-alumno.dto';
 import { UpdateAlumnoDto } from './dto/update-alumno.dto';
+import { Alumno } from './entities/alumno.entity';
+import { Model } from 'mongoose';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AlumnoService {
-  create(createAlumnoDto: CreateAlumnoDto) {
-    return 'This action adds a new alumno';
+  constructor(
+    @InjectModel(Alumno.name)
+    private readonly alumnoModel: Model<Alumno>,
+
+    private readonly configService: ConfigService,
+  ) {}
+
+  mapDtoToEntity(createAlumnoDto: CreateAlumnoDto): Partial<Alumno> {
+    return {
+      name: createAlumnoDto.name,
+      fecha1: createAlumnoDto.fecha1,
+      fecha2: createAlumnoDto.fecha2,
+      diaCobro: createAlumnoDto.diaCobro,
+      costoMensual: createAlumnoDto.costoMensual,
+      contacto: createAlumnoDto.contacto,
+      createdOn: new Date().getTime(),
+    };
+  }
+
+  async create(createAlumnoDto: CreateAlumnoDto) {
+    const newAlumno = this.mapDtoToEntity(createAlumnoDto);
+
+    try {
+      const alumno = await this.alumnoModel.create(newAlumno);
+
+      return alumno;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   findAll() {
@@ -22,5 +57,17 @@ export class AlumnoService {
 
   remove(id: number) {
     return `This action removes a #${id} alumno`;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `Alumno ya existe en la db ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(
+      `No se puede crear el Alumno - revisar server logs`,
+    );
   }
 }

12b95ea1d558115d19b1cb41069974e2c24ae4f8
luisiy01 <luisnunez91@gmail.com>
2025-10-19 14:14:26 -0600

    add docker files

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:22.12.0-alpine3.21
+
+ENV APP_PORT 3000
+ENV NODE_ENV prod
+ENV WORKDIR_APP /var/prod
+
+WORKDIR ${WORKDIR_APP}
+COPY package.json .
+RUN yarn install
+COPY . .
+
+RUN yarn run build
+
+EXPOSE ${APP_PORT}
+
+CMD ["bash", "run.sh"]
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import {
+  ValidationPipe,
+  LogLevel,
+  Logger,
+  VersioningType,
+} from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('bootstrap');
+  const logLevel: LogLevel[] = ['error', 'warn', 'fatal', 'log'];
+
+  if (process.env.NODE_ENV === 'dev') {
+    logLevel.push('debug', 'verbose');
+  }
+
+  const app = await NestFactory.create(AppModule, { logger: logLevel });
+
+  const config = new DocumentBuilder()
+    .setTitle('LM Robotica Backend')
+    .setDescription('LM Robotica API')
+    .setVersion('2.0')
+    .addTag('node')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.enableCors();
 
@@ -11,6 +34,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
+      enableDebugMessages: process.env.NODE_ENV === 'dev',
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
